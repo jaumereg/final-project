1225ccf777915eef72863001a9202a109330b7cb
Jaume Regas <jaumeregas1@gmail.com>
2017-05-30 16:44:53 +0200

    navbar added

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const express = require('express')
+// const express = require('express')
 
-const app = express()
+// const app = express()
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   require('dotenv').config()
+// }
 
-const PORT = process.env.PORT
+// const PORT = process.env.PORT
 
-app.use(express.static('public'))
+// app.use(express.static('public'))
 
-app.listen(PORT)
-console.log(`Listening on PORT ${PORT}`)
+// app.listen(PORT)
+// console.log(`Listening on PORT ${PORT}`)

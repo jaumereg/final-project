93f20c89f7f09b5c4d6afb32e448da2b0127e3b1
Jaume Regas <jaumeregas1@gmail.com>
2017-06-05 15:54:55 +0200

    match schema added

---
+++ server/models/Match.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const MatchSchema = new Schema ({
+    location: {
+      type: String,
+      required: true
+    },
+    court: {
+       type: String,
+       required: true
+    },
+    date: {
+       type: Number,
+       required: true
+    },
+    hour: {
+       type: Number,
+       required: true
+    },
+    price: {
+       type: Number,
+       required: true
+    },
+    levelFrom: {
+       type: String,
+       required: true
+    },
+    levelTo: {
+       type: String,
+       required: true
+    },
+    gender: {
+       type: String,
+       required: true
+    },
+    team1Left: {
+       type: String,
+       required: true
+    },
+    team1Right: {
+       type: String,
+       required: true
+    },
+    team2Left: {
+       type: String,
+       required: true
+    },
+    team2Right: {
+       type: String,
+       required: true
+    }
+
+
+})
+
+const Match = mongoose.model('Match', MatchSchema)
+
+module.exports = Match

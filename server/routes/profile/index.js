4d69c3e5766a75080b53136200a7225303f41680
Jaume Regas <jaumeregas1@gmail.com>
2017-06-07 23:56:22 +0200

    index profile updated

--- server/routes/profile/index.js
+++ server/routes/profile/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 const updateProfile = require('./handlers/updateProfile')
+const getProfile = require('./handlers/getProfile')
 
+router.get('/', getProfile)
 router.put('/:id', updateProfile)
 
 module.exports = router

2ff5b17dfc24fd61620420c0e0172072767060da
Jaume Regas <jaumeregas1@gmail.com>
2017-06-16 12:46:04 +0200

    modal create match updated

--- server/routes/matches/handlers/addMatch.js
+++ server/routes/matches/handlers/addMatch.js
@@ -1,7 +1,13 @@
 const Match = require( __base + 'models/Match')
 
 function addMatch( req, res ) {
-  const {location, court, date, hour, duration, price, levelFrom, gender, team1Left, team1Right, team2Left, team2Right} = req.body
+  let {location, court, date, hour, duration, price, levelFrom, gender, team1Left, team1Right, team2Left, team2Right} = req.body
+  if (!date) {
+    date = '16 - 06 - 2017'
+  }
+  if (!hour) {
+    hour = "17:00"
+  }
   const match = new Match( {location, court, date, hour, duration, price, levelFrom, gender, team1Left, team1Right, team2Left, team2Right} )
   match.save()
     .then( () => res.status(200).json({ msg: 'match inserted properly'}) )
